9dcef3d3e0b18cc98b2c0a0b6169f5b191113f9d
AzevedooS <142638112+AzevedooS@users.noreply.github.com>
2026-03-27 21:28:14 -0300

    Update Main.C

--- Parte 14/Main.C
+++ Parte 14/Main.C
@@ -1,1 +1,36 @@
 
+
+// RA:2223108777
+// Patrick dos Santos Azevedo
+
+// Parte 14
+
+#include <stdio.h>
+
+void LocalizarProduto(int precos[], int tamanho, int alvo) {
+	int encontrou = 0;
+
+	for (int indiceAtual = 0; indiceAtual < tamanho; indiceAtual++) {
+		for (int indiceComparacao = indiceAtual + 1; indiceComparacao < tamanho; indiceComparacao++) {
+			if (precos[indiceAtual] + precos[indiceComparacao] == alvo) {
+				printf("Indices encontrados: [%d, %d]\n", indiceAtual, indiceComparacao);
+				encontrou = 1;
+				return;
+			}
+		}
+	}
+
+	if (!encontrou) {
+		printf("Nenhum par foi encontrado.\n");
+	}
+}
+
+int main() {
+	int precos[] = {10, 20, 35, 50, 75};
+	int tamanho = sizeof(precos) / sizeof(precos[0]);
+	int alvo = 55;
+
+	LocalizarProduto(precos, tamanho, alvo);
+
+	return 0;
+}
